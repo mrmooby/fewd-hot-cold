9c9a9affff5e8e4e9626df11a4f42bdcdb90d56f
mrmooby <scotthayward@gmail.com>
2015-10-13 12:39:30 -0400

    initial build

--- js/app.js
+++ js/app.js
@@ -1,17 +1,54 @@
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
+$(document).ready(function()
+{
+
+/* Declare variables */
+
+var userInput;
+var unknownInteger;
+var guessCounter;
+var correctGuess = false;
+
+/* Play the game */
+
+	randomInteger();
+	UserInput();
+
+});
+
+/* Generate a random number between 1 and 100 */
+
+function randomInteger()
+{
+	unknownInteger = Math.floor(Math.random() * 101);
+	console.log("unknownInteger= " + unknownInteger);
+	return unknownInteger;
+}
+
+/* Capture user input/guesses */
+
+function UserInput()
+{
+	$("form").submit(function(){ 		
+  			userInput = $('input#userGuess.text').val();
+  			console.log("userInput= " + userInput);
+	});
+}
+
+
+/* ORIGINAL CODE BUNDLED WITH FORK - ADD BACK IN AFTER GETTING CORE PROGRAM FUNCTIONAL */
+
+	/*--- Display information modal box ---
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
 
-  	/*--- Hide information modal box ---*/
+  	/*--- Hide information modal box ---
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
+*/
 
-
+  	/* generate a random number */
